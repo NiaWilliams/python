c772e461804118564dd19ee0b072b9b7d9c5dc64
Nia Williams <nswilliams0908@gmail.com>
2018-08-28 14:45:52 +0000

    saving stuff

---
+++ practice.py
@@ -0,0 +1,14 @@
+demo_day_tix = {
+     'name': 'Ola',
+     'number_tix': '4',
+     'email': 'address@something.com',
+     'discount': False
+ }
+ 
+print(demo_day_tix)
+ 
+print(demo_day_tix['name'])
+
+demo_day_tix['student'] = 'Sean'
+
+print(demo_day_tix)
---
+++ practice2.py
@@ -0,0 +1,11 @@
+demond_salary = 525600
+arelia_salary = 135820
+
+def potato(salary):
+    if demond_salary > arelia_salary:
+        print('Demond\'s salary is greater than Arelia\'s salary.')
+    
+    # if True:
+    #     print(demond_salary + arelia_salary)
+        
+potato(7531)
